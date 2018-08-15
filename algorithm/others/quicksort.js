f2d51648ef9f8cacd2c65bc8806301f2ee2399b5
Dmitiry <gaydukov89@gmail.com>
2018-08-15 12:02:59 +0300

    add quicksort

---
+++ algorithm/others/quicksort.js
@@ -0,0 +1,35 @@
+(()=>{
+    const quickSort = (arr)=>{
+        if(arr.length == 0 || arr.length == 1){
+            return arr
+        }
+        const len = arr.length;
+        const pivotIndex = Math.floor(len/2)
+        const pivot = arr[pivotIndex]
+        const arr1 = [],
+            arr2 = [];
+        for(let i = 0; i < len; i++){
+            if(i == pivotIndex){
+                continue
+            }
+            if(arr[i] < pivot){
+                arr1.push(arr[i])
+            }
+            else{
+                arr2.push(arr[i])
+            }
+        }
+        return [...quickSort(arr1), pivot, ...quickSort(arr2)]
+    }
+
+
+
+    const arr = [7,3, 1,2, 6,9, 8,4]
+    for(let i = 0; i < 100; i++){
+        //arr.push(Math.round(Math.random()*10))
+    }
+
+    console.log(
+        quickSort(arr)
+    )
+})();
