3bca1fe5e4f635565e89ee4ce97f4116fa4df251
Dmitiry <gaydukov89@gmail.com>
2018-08-15 11:50:36 +0300

    fix road count

--- algorithm/unified-state-exam/roads-count.js
+++ algorithm/unified-state-exam/roads-count.js
@@ -1,102 +1,76 @@
 'use strict';
 
-/**
- * Task is simple recursion, we just make depth-first search and when we got to last point, we increment road number
- *
- * @param from
- * @param to
- * @param graph
- * @returns {number}
- */
-const getRoadCount = (from, to, graph) => {
-    let roads = 0;
-    if(graph[from]){
-        graph[from].map(item=>{
-            if(item == to){
-                roads++;
-            }
-            else{
-                roads += getRoadCount(item, to, graph);
-            }
-        });
+(()=>{
+    const graph = {
+        A: ["B", "C", "D"],
+        B: ["A", "C", "E", "F"],
+        C: ["A", "B", "F"],
+        D: ["A", "G"],
+        E: ["B", "G"],
+        F: ["B", "C", "G"],
+        G: ["D", "E", "F", "H"],
+        H: ["G"]
     }
-    return roads;
-}
 
-/**
- * The same as previous, only added bypass array.
- * It's for task like "how many roads from A to K, that doesn't go through D"
- * 
- * @param from
- * @param to
- * @param bypass
- * @param graph
- * @returns {number}
- */
-const getRoadCountWithBypass = (from, to, bypass, graph) => {
-    let roads = 0;
-    if(graph[from]){
-        graph[from].map(item=>{
-            if(bypass.indexOf(item) == -1){
-                if(item == to){
-                    roads++;
-                }
-                else{
-                    roads += getRoadCountWithBypass(item, to, bypass, graph);
-                }
+    const getRoadList = (from, to, graph, path=from)=>{
+        const list = []
+        const inner = (from, to, graph, path) => {
+            if(graph[from]){
+                graph[from].map(item=>{
+                    if(!path.includes(item)){
+                        const _path = path + item
+                        if(item == to){
+                            list.push(_path)
+                        }
+                        else{
+                            inner(item, to, graph, _path)
+                        }
+                    }
+                });
             }
-        });
-    }
-    return roads;
-}
-
-/**
- * The same technique with recursion
- * The only diff, is that we wrap the func, so we could return array
- *
- * @param from
- * @param to
- * @param graph
- * @param path
- * @returns {Array}
- */
-const getRoadList = (from, to, graph, path=from)=>{
-    const list = []
-    const inner = (from, to, graph, path) => {
-        if(graph[from]){
-            graph[from].map(item=>{
-                const _path = path + item
-                if(item == to){
-                    list.push(_path)
-                }
-                else{
-                    inner(item, to, graph, _path)
-                }
-            });
         }
+        inner(from, to, graph, path)
+        return list;
     }
-    inner(from, to, graph, path)
-    return list;
-}
+
+    const getRoadCount = (from, to, graph) => {
+        const list = getRoadList(from, to, graph)
+        return list.length
+    }
+
+    const getRoadCountWithBypass = (from, to, bypass, graph) => {
+        const list = getRoadList(from, to, graph)
+        let count=0
+        list.map(road=>{
+            let intersect = 0
+            bypass.map(point=>{
+                if(road.includes(point)){
+                    intersect++
+                }
+            })
+            if(intersect == 0){
+                count++
+            }
+        })
+        return count
+    }
+
+    const getShortestRoad = (from, to, graph) => {
+        const list = getRoadList(from, to, graph)
+        let short = list[0]
+        let min = list[0].length
+        list.map(item=>{
+            const len = item.length
+            if(min > len){
+                short = item
+                min = len
+            }
+        })
+        return short;
+    }
 
 
-/**
- * Given a Graph like this find how many roads from A to K
- */
-const graph = {
-    "a": ["б", "в", "г"],
-    "б": ["в", "д"],
-    "в": ["д", "ж"],
-    "г": ["в", "е"],
-    "д": ["и", "к"],
-    "е": ["ж", "к"],
-    "ж": ["к"],
-    "и": ["к"],
-    "к": [],
-}
-
-console.log(
-    getRoadCount("a", "к", graph),
-    getRoadCountWithBypass("a", "к", ["б"], graph),
-    getRoadList("a", "к", graph),
-)
+    console.log(
+        getRoadCountWithBypass("A", "H", ["C"],  graph)
+    )
+})();
