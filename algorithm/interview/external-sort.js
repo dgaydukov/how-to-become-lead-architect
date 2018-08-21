0f0627aaeb2a36d64a2fb42a3e3da394fabcb25c
Dmitiry <gaydukov89@gmail.com>
2018-08-21 11:47:49 +0300

    done sort

--- algorithm/interview/external-sort.js
+++ algorithm/interview/external-sort.js
@@ -4,7 +4,13 @@
  *
  * This is the general task called external sorting
  * https://en.wikipedia.org/wiki/External_sorting
+ *
+ *
+ * For simple reason i assume that 1mb is 1 number from 0 to 100
+ * To execute wiki example just run externalSort(900, 100
  */
+
+
 
 (()=>{
     const isSorted = (arr)=>{
@@ -16,7 +22,7 @@
         }
         return true
     }
-    const sort = (arr)=>{
+    const quickSort = (arr)=>{
         const sorted = JSON.parse(JSON.stringify(arr))
         sorted.sort((a,b)=>a-b)
         return sorted
@@ -28,73 +34,81 @@
         }
         return arr
     }
-    const nListMergeSort = (arr)=>{
-        const len = arr.length
-        const list = JSON.parse(JSON.stringify(arr))
-        const sorted = []
-        const isEmpty = (arr)=>{
-            const len = arr.length
-            for(let i = 0; i < len; i++){
-                if(arr[i].length > 0){
-                    return false
-                }
-            }
-            return true;
+
+
+    const externalSort = (fileLen, ramLen)=>{
+        const disk = {
+            list: {},
+            sorted: []
         }
-        while(!isEmpty(list)){
+        const arr = generateList(fileLen)
+        const loops = fileLen%ramLen == 0 ? fileLen/ramLen : Math.floor(fileLen/ramLen) + 1
+        for(let i = 0; i < loops; i++){
+            disk.list[i] = quickSort(arr.slice(i * ramLen, (i+1) * ramLen))
+        }
+        const inputBufferCellLen = Math.floor(ramLen/2/loops)+1
+        const outputBufferLen = ramLen - loops * inputBufferCellLen
+
+
+        const inputBuffer = {}
+        let outputBuffer = []
+
+        //initial filling
+        for(let i = 0; i < loops; i++){
+            inputBuffer[i] = []
+            let j = 0
+            while(j < inputBufferCellLen){
+                const item = disk.list[i].splice(0, 1)
+                inputBuffer[i].push(item[0])
+                j++
+            }
+        }
+
+
+        for(let counter = 0; counter < fileLen; counter++){
             let min = Number.MAX_SAFE_INTEGER;
             let index = 0;
-            for(let i = 0; i < len; i++){
-                const sub = list[i]
+            for(let i = 0; i < loops; i++){
+                const sub = inputBuffer[i]
                 if(min > sub[0]){
                     min = sub[0]
                     index = i
                 }
             }
-            list[index].splice(0, 1)
-            sorted.push(min)
-        }
-        return sorted
-    }
+            inputBuffer[index].splice(0, 1)
+            outputBuffer.push(min)
 
-
-    const arrLen = 900
-    const ramLen = 100
-
-    const arr = generateList(arrLen)
-    const disk = {
-        list: {},
-        sorted: []
-    }
-
-    const externalSort = (arr)=>{
-        const loops = arrLen%ramLen == 0 ? arrLen/ramLen : Math.floor(arrLen/ramLen) + 1
-        for(let i = 0; i < loops; i++){
-            disk.list[i] = sort(arr.slice(i * ramLen, (i+1) * ramLen))
-        }
-        const inputBufferLen = ramLen%(loops+1) == 0 ? ramLen/(loops+1) : Math.floor( ramLen/(loops+1)) + 1
-        const outputBufferLen = ramLen - loops * inputBufferLen
-
-
-        let counter = 0
-        while(disk.sorted.length < arrLen){
-            if(counter==1){
-                break
+            //if any bucket empty fill it
+            for(let i = 0; i < loops; i++){
+                if(inputBuffer[i].length == 0){
+                    let j = 0
+                    while(j < inputBufferCellLen){
+                        if(disk.list[i].length == 0){
+                            break
+                        }
+                        const item = disk.list[i].splice(0, 1)
+                        inputBuffer[i].push(item[0])
+                        j++
+                    }
+                }
             }
-            counter++
-            const inputBuffer = []
-            const outputBuffer = []
 
-
-            disk.sorted = [].concat(disk.sorted, sorted)
+            // if buffer full or we have last iteration, write to disk
+            if(outputBuffer.length == outputBufferLen || counter == fileLen-1){
+                disk.sorted = [].concat(disk.sorted, outputBuffer)
+                outputBuffer = []
+            }
         }
-        return disk.sorted
+
+        return {original: arr, sorted: disk.sorted}
     }
 
-
-    const sorted = externalSort(arr)
+    const fileLen = 64
+    const ramLen = 8
+    const sorted = externalSort(fileLen, ramLen)
     console.log(
-        //sorted,
-        //isSorted(sorted)
+        sorted,
+        isSorted(sorted.original),
+        isSorted(sorted.sorted),
     )
 })()
