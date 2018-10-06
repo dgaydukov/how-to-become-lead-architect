6e49be57c492692cc333e92d30f7c66489ef33a1
Dmitry Gaydukov <gaydukov89@gmail.com>
2018-10-06 17:16:03 +0300

    add new task, fix

---
+++ interview/js/async-await-for-vs-map.js
@@ -0,0 +1,26 @@
+// const list = [1, 2, 3, 4, 5]
+
+// const asyncFunc = (n) => {
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             resolve(n)
+//         }, 1000)
+//     })
+// }
+
+// const asyncLoop = async() => {
+//     for(const i in list){
+//         const result = await asyncFunc(list[i])
+//         console.log(result)
+//     }
+// }
+
+// const asyncLoop2 = async() => {
+//     list.map(async v => {
+//         const result = await asyncFunc(v)
+//         console.log(result)
+//     })
+// }
+ 
+
+// asyncLoop2() 
