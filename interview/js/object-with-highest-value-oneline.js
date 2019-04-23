7d9ff379e1f219a6a6fc990476d6f7b28e6605be
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-04-23 17:49:18 +0800

    add task

---
+++ interview/js/object-with-highest-value-oneline.js
@@ -0,0 +1,11 @@
+const arr = [
+    {a: 2, index: 11},
+    {a: 3, index: 4},
+    {a: 5, index: 5},
+    {a: 1, index: 1},
+    {a: 6, index: 6},
+  ];
+  
+  const obj = arr.reduce((acc, curr) => acc.index > curr.index ? curr : acc, arr[0]);
+  
+  console.log(obj);
