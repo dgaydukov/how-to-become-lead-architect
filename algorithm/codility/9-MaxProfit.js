325b761a8ac15d64ae3ac869da0686c849ded301
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-07-19 19:05:33 +0800

    rewrite to 100% perfomance MaxSliceSum

--- algorithm/codility/9-MaxProfit.js
+++ algorithm/codility/9-MaxProfit.js
@@ -32,7 +32,7 @@
 (() => {
     /**
      * More advanced solution based on Kadane's algorithm (maxi subarray sum)
-     * see below for implementation
+     * **see MaxSliceSum for example implementation
      *
      * @param arr
      * @returns {number}
@@ -53,36 +53,5 @@
 
     console.log(
         solution([23171, 21011, 21123, 21366, 21013, 21367])
-    );
-})();
-
-(() => {
-    /**
-     * The idea is super simple once you get it.
-     * Basically we calculate sequence as long as it sum greater than 0. Otherwise we start from beginning.
-     * Cause if later we got big sequese, previous less than 0 will only decrement it, se we can get rid of it
-     * 
-     * @param {*} arr 
-     */
-    const maxSubArraySum = arr => {
-        let sum = 0, max = 0, start = 0, end = 0;
-        for (let i = 1; i < arr.length; i++) {
-            sum += arr[i];
-            if(sum < 0){
-                sum = 0;
-                start = i + 1;
-            }
-            if(max < sum){
-                end = i;
-                max = sum;
-            }
-        }
-        console.log({start, end});
-        return max;
-    }
-
-
-    console.log(
-        maxSubArraySum([5, -7, 3, 5, -2, 4, -1])
     );
 })();
--- algorithm/codility/9-MaxSliceSum.js
+++ algorithm/codility/9-MaxSliceSum.js
@@ -1,42 +1,49 @@
-'use strict';
-
-
-/**
- * Smart way - count the sum of all positive numbers
- *
- * @param arr
- * @returns {number}
- */
-const solution = (arr) => {
-    const len = arr.length;
-    let sum = 0,
-        maxSum = Number.MIN_SAFE_INTEGER,
-        totalSum = 0;
-    for(let i = 0; i < len; i++){
-        totalSum += arr[i];
-        if(arr[i] > 0){
+(() => {
+    /**
+     * https://en.wikipedia.org/wiki/Maximum_subarray_problem
+     * The idea is super simple once you get it.
+     * Basically we calculate sequence as long as it sum greater than 0. Otherwise we start from beginning.
+     * Cause if later we got big sequese, previous less than 0 will only decrement it, se we can get rid of it
+     * 
+     * 
+     * @param {*} arr 
+     */
+    const solution = arr => {
+        let sum = arr[0],
+            max = sum,
+            min = sum,
+            noZero = true;
+        for (let i = 1; i < arr.length; i++) {
             sum += arr[i];
-            if(maxSum < sum){
-                maxSum = sum;
+            if (sum < 0) {
+                sum = 0;
+            }
+            if (sum < arr[i]) {
+                sum = arr[i]
+            }
+            if (max < sum) {
+                max = sum;
+            }
+            if (min < arr[i]) {
+                min = arr[i];
+            }
+            if (arr[i] == 0) {
+                noZero = false;
             }
         }
-        else{
-            sum = 0;
-            if(maxSum < arr[i]){
-                maxSum = arr[i];
-            }
+        if (max == 0 && noZero) {
+            max = min;
         }
+        return max;
     }
-    if(maxSum < totalSum){
-        maxSum = totalSum;
-    }
-    return maxSum;
-}
 
 
-console.log(
-    solution([3, 2, -6, 4, 0]),
-    solution([10]),
-    solution([-10]),
-    solution([3, -2, 3])
-);
+    console.log(
+        solution([5, -7, 3, 5, -2, 4, -1]),
+        solution([3, 2, -6, 4, 0]),
+        solution([-2, -3, -4, -1]),
+        solution([-4, 6, -2]),
+        solution([3, -2]),
+
+    );
+})();
