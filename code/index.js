1fac983f3553b56b236ed67722120306ec00f96a
diman <gaydukov89@gmail.com>
2019-08-02 22:20:03 +0800

    add dp version of fibfrog

--- code/index.js
+++ code/index.js
@@ -88,7 +88,174 @@
         // solution([1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0]) == 3,
         // solution([1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0]) == 3,
 
-        solution([1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0])
+        //solution([1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0])
+
+    )
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(() => {
+    const solution = (arr) => {
+        const size = arr.length,
+            leaves = [],
+            fibHash = {},
+            F = [1, 1];
+        for (let i = 2; i < size + 2; i++) {
+            F[i] = F[i - 1] + F[i - 2];
+            if (F[i] > size) {
+                break;
+            }
+        }
+        for (let i = 0; i < F.length; i++) {
+            fibHash[F[i]] = 1;
+        }
+        if (fibHash[size + 1]) {
+            return 1;
+        }
+        for (let i = 0; i < size; i++) {
+            if (arr[i] == 1) {
+                leaves.push(i + 1);
+            }
+        }
+        leaves.push(size + 1);
+        const len = leaves.length;
+        const mins = [];
+        for(let i = 0; i < len; i++){
+            let min = Number.MAX_SAFE_INTEGER;
+            if(fibHash[leaves[i]]){
+                min = 1;
+            }
+            else{
+                for(let j = 0; j < i; j++){
+                    if(mins[j] != -1){
+                        const fib = fibHash[leaves[i]-leaves[j]];
+                        //console.log(i, leaves[i], leaves[j], fib)
+                        if(fib && min > mins[j]+1){
+                            min = mins[j]+1;
+                        }
+                    }
+                }
+            }
+            if(min == Number.MAX_SAFE_INTEGER){
+                min = -1;
+            }
+            mins[i] = min;
+        }
+        //console.log(leaves, mins)
+        return mins[len-1] || -1;
+    }
+
+    console.log(
+        // solution([0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0]) == 3,
+        // solution([0, 0, 0]) == -1,
+        // solution([]) == 1,
+        // solution([1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0]) == 3,
+        // solution([1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0]) == 3,
+
+        solution([0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0])
 
     )
 })();
