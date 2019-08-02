318c48b5af283fd4d4d792857f7d912abbd05019
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-08-02 12:21:03 +0800

    add base folder for basic algorithms

--- code/index.js
+++ code/index.js
@@ -1,112 +1,77 @@
 (() => {
     /**
-     * Knapsack DP solution.
-     * Create 2D array of size+1 (as i/row) and weight+1 (as j/column), init all with 0.
-     * First row and column will stay at 0. For no item, for every weight from 0 to weight the max value would be 0
-     * The save true to first column. For every item for 0 weight the max value would be 0
+     * Solution to count 
      * 
      * @param {*} arr 
      * @param {*} weight 
      */
-    const solution = (arr, weight) => {
-        const size = arr.length;
-        const res = [];
-        for (let i = 0; i <= size; i++) {
-            res[i] = [];
-            for (let j = 0; j <= weight; j++) {
-                res[i][j] = 0;
-            }
-        }
-        for (let i = 1; i <= size; i++) {
-            const w = arr[i - 1].w,
-                v = arr[i - 1].v;
-            for (let j = 1; j <= weight; j++) {
-                res[i][j] = w > j ? res[i - 1][j] : Math.max(res[i - 1][j], res[i - 1][j - w] + v);
-            }
-        }
-        return res[size][weight];
-    }
-
-
-    console.log(
-        // solution([{ w: 3, v: 10 }, { w: 1, v: 3 }, { w: 2, v: 9 }, { w: 2, v: 5 }, { w: 1, v: 6 }], 6) == 25,
-        // solution([{ w: 3, v: 10 }, { w: 1, v: 3 }, { w: 3, v: 9 }, { w: 2, v: 5 }, { w: 1, v: 6 }], 6) == 19,
-        // solution([
-        //     { w: 70, v: 135 },
-        //     { w: 73, v: 139 },
-        //     { w: 77, v: 149 },
-        //     { w: 80, v: 150 },
-        //     { w: 82, v: 156 },
-        //     { w: 87, v: 163 },
-        //     { w: 90, v: 173 },
-        //     { w: 94, v: 184 },
-        //     { w: 98, v: 192 },
-        //     { w: 106, v: 201 },
-        //     { w: 110, v: 210 },
-        //     { w: 113, v: 214 },
-        //     { w: 115, v: 221 },
-        //     { w: 118, v: 229 },
-        //     { w: 120, v: 240 },
-        // ], 750) == 1458,
-
-        solution([{ w: 3, v: 10 }, { w: 1, v: 3 }, { w: 3, v: 9 }, { w: 2, v: 5 }, { w: 1, v: 6 }], 6),
-    );
-});
-
-(()=>{
-    const solution = (arr, weight) => {
-        const size = arr.length;
-        arr.sort((a,b)=>b.v/b.w-a.v/a.w);
-        console.log(arr)
+    const greedyCount = (arr, weight) => {
+        const sorted = [...arr];
+        sorted.sort((a, b) => b.v / b.w - a.v / a.w);
         let maxPrice = 0, index = 0;;
-        while(weight > 0){
-            if(arr[index].w <= weight){
-                weight -= arr[index].w;
-                maxPrice += arr[index].v;
+        while (weight > 0) {
+            const item = sorted[index];
+            if (weight >= item.w) {
+                weight -= item.w;
+                maxPrice += item.v;
             }
-            else{
+            else {
                 index++;
             }
         }
         return maxPrice;
     }
 
-    const solution2 = (arr, weight) => {
-        if(weight <= 0){
-            return 0;
-        }
-        let max = 0;
-        for(let i = 0; i < weight; i++){
-            max = Math.max(max, arr[i].v + solution2(arr, weight-i-1))
-            console.log({i, weight, v: arr[i].v, max})
-        }
-        console.log(`before return weight: ${weight}, max: ${max}`)
-        return max;
-    }
-
-    const solution3 = (arr, weight) => {
+    const dpCount = (arr, weight) => {
         const F = [0];
-        for(let i = 1; i <= weight; i++){
+        for (let i = 1; i <= weight; i++) {
             let max = 0;
-            for(let j = 1; j <= i; j++){
-                const v = arr[j-1].v + F[i-j];
-                if(max < v){
+            for (let j = 1; j <= i; j++) {
+                const v = arr[j - 1].v + F[i - j];
+                if (max < v) {
                     max = v;
                 }
             }
             F[i] = max;
         }
-        console.log(F)
         return F[weight];
     }
+
+    let diff = 0;
+    let max = 0;
+    for (let i = 0; i < 10 ** 6; i++) {
+        const n = 6, arr = [];
+        for (let j = 0; j < n; j++) {
+            arr.push({ w: j + 1, v: Math.round(Math.random() * (j + n)) || 1 });
+        }
+        const r1 = solution(arr, n);
+        const r2 = rodCount(arr, n);
+        if (r1 != r2) {
+            if (max < Math.abs(r1 - r2)) {
+                max = Math.abs(r1 - r2);
+            }
+            //console.log('got it', arr, r1, r2);
+            diff++;
+            //break;
+        }
+    }
+    console.log(`diff: ${diff}, max: ${max}`)
 
     console.log(
         // solution([{w:1, v:1},{w:2, v:5},{w:3, v:8},{w:4, v:9},{w:5, v:10},{w:6, v:17},{w:7, v:17},{w:8, v:20}], 8)==22,
         // solution([{w:1, v:3},{w:2, v:5},{w:3, v:8},{w:4, v:9},{w:5, v:10},{w:6, v:17},{w:7, v:17},{w:8, v:20}], 8)==24,
 
-
-        //solution2([{w:1, v:1},{w:2, v:6},{w:3, v:8},{w:4, v:9}], 4),
-        solution3([{w:1, v:1},{w:2, v:6},{w:3, v:8},{w:4, v:9}], 4),
+        // solution([
+        //     { w: 1, v: 1 },
+        //     { w: 2, v: 5 },
+        //     { w: 3, v: 7 },
+        //     { w: 4, v: 4 },
+        //     { w: 5, v: 8 }], 5),
+        // rodCount([{ w: 1, v: 1 },
+        //     { w: 2, v: 5 },
+        //     { w: 3, v: 7 },
+        //     { w: 4, v: 4 },
+        //     { w: 5, v: 8 }], 5),
     )
 })();
 
