33a8081cde120cf61f3b5cd7e19d5d96ca618f3c
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-08-05 15:14:17 +0800

    update fibfrog to dp

--- code/index.js
+++ code/index.js
@@ -1,81 +1,61 @@
 (() => {
     /**
-     * Naive approach to brute force all possible values
-     * Method is very simple, we go from the end and check can we get to the end in one step
-     * 
-     * https://app.codility.com/demo/results/trainingMMATA3-GYH/
+     * Naive DP approach. For every leaf we calculate min number of steps to get there, or write -1, if there is no steps
+     * Take for example default array [4, 5, 7, 12] => [-1, 1, 2, 3]
+     * There is no step to get to 4
+     * there is 1 step to get to 5
+     * there are 2 steps to get to 7 (one from 5 + 1 to 5 itself)
+     * there are 3 steps to get to 12 (1 from 7 + 2 to 7 itself)
+     * Unfortunately time complexity is O(n**2). Cause for every leaf we shold check it against all previous leaves
      * 
      * @param {*} arr 
      */
     const solution = (arr) => {
         const size = arr.length,
             leaves = [],
-            leafPos = {},
-            badPos = {},
-            fibHash = { 1: 1 },
+            fibHash = {},
             F = [1, 1];
-        // calculate fibonacci numbers
         for (let i = 2; i < size + 2; i++) {
             F[i] = F[i - 1] + F[i - 2];
-            fibHash[F[i]] = 1;
             if (F[i] > size) {
                 break;
             }
         }
+        for (let i = 0; i < F.length; i++) {
+            fibHash[F[i]] = 1;
+        }
         if (fibHash[size + 1]) {
             return 1;
         }
-        // calculate leaves positions
         for (let i = 0; i < size; i++) {
             if (arr[i] == 1) {
                 leaves.push(i + 1);
             }
         }
         leaves.push(size + 1);
-        for (let i = 0; i < leaves.length; i++) {
-            leafPos[leaves[i]] = i;
-        }
-        let index = leaves.length - 2, min = Number.MAX_SAFE_INTEGER, stack = [], lastCheckedIndex = index;
-        console.log(leaves)
-        while (index >= 0) {
-            if (badPos[index]) {
-                index--;
-                continue;
-            }
-            lastCheckedIndex = Math.min(lastCheckedIndex, index);
-            const fib = leaves[index] - (stack[stack.length - 1] || 0);
-            if (fibHash[fib]) {
-                stack.push(leaves[index]);
-                if (stack.length == 1) {
-                    lastCheckedIndex = leaves.length - 1;
-                }
-                if (index == leaves.length - 1) {
-                    min = Math.min(min, stack.length);
-                    console.log({stack})
-                    const firstPosition = leafPos[stack[0]];
-                    if (firstPosition == lastCheckedIndex - 1) {
-                        //badPos[firstPosition] = 1;
-                        index = firstPosition - 1;
-                        stack = [];
-                    }
-                    else {
-                        while (leafPos[stack.pop()] != lastCheckedIndex) { }
-                        index = lastCheckedIndex - 1;
-                    }
-                }
-                else {
-                    index = leaves.length - 1;
-                }
+        const len = leaves.length;
+        const mins = [];
+        for (let i = 0; i < len; i++) {
+            let min = Number.MAX_SAFE_INTEGER;
+            if (fibHash[leaves[i]]) {
+                min = 1;
             }
             else {
-                index--;
+                for (let j = 0; j < i; j++) {
+                    if (mins[j] != -1) {
+                        const fib = fibHash[leaves[i] - leaves[j]];
+                        if (fib && min > mins[j] + 1) {
+                            min = mins[j] + 1;
+                        }
+                    }
+                }
             }
-            if (index < leafPos[stack[stack.length - 1]]) {
-                badPos[leafPos[stack[stack.length - 1]]] = 1;
-                stack.pop();
+            if (min == Number.MAX_SAFE_INTEGER) {
+                min = -1;
             }
+            mins[i] = min;
         }
-        return min == Number.MAX_SAFE_INTEGER ? -1 : min;
+        return mins[len - 1] || -1;
     }
 
     console.log(
@@ -84,27 +64,9 @@
         // solution([]) == 1,
         // solution([1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0]) == 3,
         // solution([1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0]) == 3,
+        // solution([ 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0 ]) == 4,
 
-        solution([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+        solution([0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0])
 
     )
-
-
-    // const arr = [0];
-    // for(let i = 0; i < 13; i++){
-    //     const size = arr.length;
-    //     for(let j = 0; j < size; j++){
-    //         arr.push(Number(!arr[j]));
-    //     }
-    // }
-    // console.log(solution(arr));
-
-    // const s = + new Date();
-    // const arr = [];
-    // for(let i = 0; i < 10**5; i++){
-    //     arr.push(1);
-    // }
-    // console.log(solution(arr));
-    // const e = + new Date() - s;
-    // console.log(`time taken: ${e/1000}`);
 })();
