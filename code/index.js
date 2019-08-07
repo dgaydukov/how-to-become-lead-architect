6c7ed125bb8c770db2b94a1ef06a64cc716a9ae3
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-08-07 12:57:21 +0800

    add nailing problem solution 100%correct but slow

--- code/index.js
+++ code/index.js
@@ -1,20 +1,32 @@
 (() => {
+    /**
+     * Naive approach with time complexity O(n*m)
+     * Basically for every nail we check, can we found interval. If yes, we mark this interval as visited
+     * And continue until we mark all intervals or finish our nails
+     * 
+     * https://app.codility.com/demo/results/training28K7BE-37V/
+     * 
+     * @param {*} A 
+     * @param {*} B 
+     * @param {*} C 
+     */
     const solution = (A, B, C) => {
         const pSize = A.length,
-            nSize = C.length;
+            nSize = C.length,
+            arr = [];
         let nails = 0,
-            planks = 0,
-            pStart = 0;
+            planks = 0;
+        for (let i = 0; i < pSize; i++) {
+            arr.push({ start: A[i], end: B[i], visited: false })
+        }
+        const len = arr.length;
         for (let i = 0; i < nSize; i++) {
             nails++;
             const nail = C[i];
-            for (let j = pStart; j < pSize; j++) {
-                if (nail >= A[j] && nail <= B[j]) {
+            for (let j = 0; j < len; j++) {
+                if (nail >= arr[j].start && nail <= arr[j].end && !arr[j].visited) {
+                    arr[j].visited = true;
                     planks++;
-                }
-                else {
-                    pStart = j;
-                    break;
                 }
             }
             if (planks == pSize) {
@@ -28,7 +40,8 @@
         // solution([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10, 2]) == 4,
         // solution([2], [2], [1]) == -1,
         // solution([3, 3], [4, 4], [3, 4]) == 1,
+        // solution([1, 4, 5, 8], [4, 5, 9, 10], [10, 4, 6, 7, 2]) == 3,
 
-        solution([1, 4, 5, 6, 8], [4, 5, 9, 7, 10], [4, 6, 7, 10, 2]),
+        solution([1, 4, 5, 8], [4, 5, 9, 10], [10, 4, 6, 7, 2])
     )
 })();
