1f9a747a324a3594708ac29768cebc016b41ea37
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-08-07 16:18:34 +0800

    add more efficient solution to speed, up to 2 times

--- code/index.js
+++ code/index.js
@@ -1,10 +1,9 @@
 (() => {
     /**
-     * Naive approach with time complexity O(n*m)
-     * Basically for every nail we check, can we found interval. If yes, we mark this interval as visited
-     * And continue until we mark all intervals or finish our nails
+     * We can approve the situation a little bit, to O(n*m/2)
+     * The point is that whenever we have found interval we remove it from array, so next iteration we don't check it anymore
      * 
-     * https://app.codility.com/demo/results/training28K7BE-37V/
+     * https://app.codility.com/demo/results/trainingP6Z5SD-H2R/
      * 
      * @param {*} A 
      * @param {*} B 
@@ -14,22 +13,19 @@
         const pSize = A.length,
             nSize = C.length,
             arr = [];
-        let nails = 0,
-            planks = 0;
+        let nails = 0;
         for (let i = 0; i < pSize; i++) {
-            arr.push({ start: A[i], end: B[i], visited: false })
+            arr.push([A[i], B[i]]);
         }
-        const len = arr.length;
         for (let i = 0; i < nSize; i++) {
             nails++;
             const nail = C[i];
-            for (let j = 0; j < len; j++) {
-                if (nail >= arr[j].start && nail <= arr[j].end && !arr[j].visited) {
-                    arr[j].visited = true;
-                    planks++;
+            for (let j = arr.length - 1; j >= 0; j--) {
+                if (nail >= arr[j][0] && nail <= arr[j][1]) {
+                    arr.splice(j, 1);
                 }
             }
-            if (planks == pSize) {
+            if (arr.length == 0) {
                 return nails;
             }
         }
