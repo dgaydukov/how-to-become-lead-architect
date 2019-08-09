b654312d3c1f90dbc547e23b16ecd54dda2a8ca4
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-08-09 10:49:53 +0800

    add minmax task

--- code/index.js
+++ code/index.js
@@ -1,33 +1,40 @@
 (() => {
-    const solution = (k, max, arr) => {
+    const getParts = (arr, max) => {
         const size = arr.length;
-        let _k = k+1, subSum = 0;
-        max = Number.MIN_SAFE_INTEGER;
-        for(let i = 0; i < size; i++){
+        let parts = 1,
+            subSum = 0;;
+        for (let i = 0; i < size; i++) {
+            subSum += arr[i];
+            if (subSum > max) {
+                parts++;
+                subSum = arr[i];
+            }
+        }
+        return parts;
+    }
+    const solution = (k, m, arr) => {
+        const size = arr.length;
+        let max = arr[0];
+        for (let i = 1; i < size; i++) {
             max = Math.max(max, arr[i]);
         }
-        while (_k > k) {
-            _k = 1;
-            for (let i = 0; i < size; i++) {
-                subSum += arr[i];
-                if (subSum > max) {
-                    _k++;
-                    subSum = 0;
-                    i--;
-                }
+        let parts = getParts(arr, max);
+        if (parts > k) {
+            while (parts != k) {
+                max++;
+                parts = getParts(arr, max);
             }
-            console.log(_k, k , max)
-            max++;
         }
-        return max-1;
+        return max;
     }
 
     console.log(
-        // solution(3, 5, [5, 3]) == 5,
         // solution(3, 5, [2, 1, 5, 1, 2, 2, 2]) == 6,
+        // solution(3, 5, [5, 3]) == 5,
         // solution(1, 1, [0]) == 0,
         // solution(1, 10, [5, 3]) == 8,
+        // solution(1, 10000, [10000]) == 10000,
 
-        solution(1, 10, [5, 3])
+        solution(3, 100, [10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
     )
-});
+})();
