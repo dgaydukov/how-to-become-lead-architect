01fb86ebf821dbbaf001d39554f8be73299dd305
Dmitriy Gaydukov <gaydukov89@gmail.com>
2019-10-17 16:20:36 +0800

    add comments

--- interview/js/throttle-vs-debounce.js
+++ interview/js/throttle-vs-debounce.js
@@ -1,3 +1,19 @@
+/*
++--------------+-------------------+-------------------+
+|              |  Throttle 1 sec   |  Debounce 1 sec   |
++--------------+-------------------+-------------------+
+| Delay        | no delay          | 1 sec delay       |
+|              |                   |                   |
+| Emits new if | last was emitted  | there is no input |
+|              | before 1 sec      |  in last 1 sec    |
++--------------+-------------------+-------------------+
+
+Examples:
+
+Search bar - Don't want to search every time user presses key? Want to search when user stopped typing for 1 sec. Use debounce 1 sec on key press.
+Shooting game - Pistol take 1 sec time between each shot but user click mouse multiple times. Use throttle on mouse click.
+
+*/
 (()=>{
     const throttle = (func, time) => {
         let callFunc = true;
