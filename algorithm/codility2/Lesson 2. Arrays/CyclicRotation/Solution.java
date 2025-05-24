8ee98b2acbfd5d2a1f49306877be975c8ff2b67e
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-05-24 21:59:45 +0300

    add rotation algos

--- algorithm/codility2/Lesson 2. Arrays/CyclicRotation/Solution.java
+++ algorithm/codility2/Lesson 2. Arrays/CyclicRotation/Solution.java
@@ -1,7 +1,14 @@
 package com.codility.java;
 
+/**
+ * Here is array rotation algo is used
+ * I use the most simple approach with additional array. So my space complexity is O(n) because additional array is created
+ * There are algorithms where you can rotate without another array, all inside current one. They may be helpful if space is an issue.
+ * Because if you have space problem, attempt to create second array with same size may throw OutOfMemoryException
+ */
 class Solution {
     public int[] solution(int[] arr, int n) {
+        // return immediately for edge cases
         if (n == arr.length || n == 0 || arr.length == 0 || arr.length == 1){
             return arr;
         }
